9a2381520267f99594876da18e929860b0d837fc
johnson452 <grj@princeton.edu>
2023-05-15 15:25:01 -0400

    BIG FIX: spline interpolation significantly improved energy conservation

--- interp_center_to_edge.m
+++ interp_center_to_edge.m
@@ -1,4 +1,4 @@
-function [x_interp] = interp_center_to_edge(x)
+function [y_interp] = interp_center_to_edge(y)
 % interp_center_to_edge() Procedure
 %[  A   B   C   D  ] - Centered Values
 %    \ / \ / \ /
@@ -6,21 +6,32 @@ function [x_interp] = interp_center_to_edge(x)
 % P = 0  - Needs to be updated elsewhere 
 %   = (OLD) (A + D)/2 
 
-%interpolate the edge cells to centered
-Nx = max(size(x));
-if Nx ~= 1
-x_interp = zeros(1,Nx+1);
-for i = 1:Nx-1
-    x_interp(i+1) = (x(i) + x(i+1))/2;
-end
 
-%Handle the edge case: Periodic, but 
-% is overwritten by BC is non-periodic
-x_interp(1) = 0; %(x(1)+x(Nx))/2;
-x_interp(Nx+1) = 0; %(x(1)+x(Nx))/2;
-else
-    x_interp = x;
-end
+% Pad y with perioid conditions
+NX = max(size(y));
+y = [y(NX),y,y(1)];
+Nx = max(size(y));
+x = linspace(0,1,Nx);
+dx = x(2)-x(1);
+x2 = linspace(0+dx/2,1-dx/2,Nx-1);
+y_interp = interp1(x,y,x2,'spline');
+
+
+% %interpolate the edge cells to centered
+% Nx = max(size(x));
+% if Nx ~= 1
+% x_interp = zeros(1,Nx+1);
+% for i = 1:Nx-1
+%     x_interp(i+1) = (x(i) + x(i+1))/2;
+% end
+% 
+% %Handle the edge case: Periodic, but 
+% % is overwritten by BC if non-periodic
+% x_interp(1) = 0; %(x(1)+x(Nx))/2;
+% x_interp(Nx+1) = 0; %(x(1)+x(Nx))/2;
+% else
+%     x_interp = x;
+% end
 
 
 end
--- interp_edge_to_center.m
+++ interp_edge_to_center.m
@@ -1,10 +1,22 @@
-function [x_interp] = interp_edge_to_center(x)
+function [y_interp] = interp_edge_to_center(y)
+% interp_edge_to_center() Procedure
+%[A   B   C   D   E] - Edge Values (x)
+%  \ / \ / \ / \ /
+%[  X   Y   Z   S  ] - Centered Values (x2)
+
 
 %interpolate the edge cells to centered
-Nx = max(size(x));
-x_interp = zeros(1,Nx-1);
-for i = 1:Nx-1
-    x_interp(i) = (x(i) + x(i+1))/2;
-end
+Nx = max(size(y));
+x = linspace(0,1,Nx);
+dx = x(2)-x(1);
+x2 = linspace(0+dx/2,1-dx/2,Nx-1);
+y_interp = interp1(x,y,x2,'spline');
+
+% Manual interp (linear) just turrible
+% Nx = max(size(x));
+% x_interp = zeros(1,Nx-1);
+% for i = 1:Nx-1
+%     x_interp(i) = (x(i) + x(i+1))/2;
+% end
 
 end
